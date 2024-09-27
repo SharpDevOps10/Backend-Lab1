4cdb122402df92636ac48bcc5878da098c8e0dec
SharpDevOps10 <td090833@gmail.com>
2024-09-27 20:41:16 +0300

    Add healthcheck endpoint with status and date

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/healthcheck', async (req, res) => {
+  const response = {
+    status: 'The server is running',
+    date: new Date().toISOString(),
+  };
+
+  res.code(200).send(response);
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log('Server is running on http://localhost:3000');
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
